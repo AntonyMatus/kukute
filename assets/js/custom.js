461e5177a1b0000892868e5e79c708c3211215d3
tony_ <antonyrebolledo@gmail.com>
2023-04-19 16:01:55 -0600

    actividades 19-04

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -131,5 +131,42 @@
         type: 'iframe'
     });
 
+    // form-contact
+    var formulario = document.querySelector('#formulario');
 
+    formulario.addEventListener('submit', function(e){
+        e.preventDefault();
+        email();
+    });
+
+    function email() {
+        let datos = new FormData(formulario);
+        fetch(`../../assets/email-templates/contact-form.php`, {
+            method: 'POST',
+            body: datos
+        }).then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if(data === 'exito'){
+                Swal.fire({
+                    position: "center",
+                    icon: "success",
+                    title: "Exito",
+                    text: "El mensaje ha sido enviado con exito!",
+                    showConfirmButton: false,
+                    timer: 3500
+                });
+                formulario.reset();
+            } else { 
+                Swal.fire({
+                    position: "center",
+                    icon: "error",
+                    title: "Oops",
+                    text: "Error al enviar el mensaje",
+                    showConfirmButton: false,
+                    timer: 3500
+                });
+            }
+        })
+    }
 }(jQuery));
